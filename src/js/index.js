a4504a919d21ce4127739b4515fca8f31ae7cfcd
Haohan Yang <yanghaohanmail@gmail.com>
2023-10-04 15:46:35 +0200

    add parceljs

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+import { Elm } from "../elm/Main.elm";
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+const storedState = localStorage.getItem("elm-todo");
+const startingState = storedState ? JSON.parse(storedState) : null;
+const app = Elm.Main.init({ flags: startingState, node: document.getElementById("root") });
+
+app.ports.setStorage.subscribe(function (state) {
+    localStorage.setItem("elm-todo", JSON.stringify(state));
+});
