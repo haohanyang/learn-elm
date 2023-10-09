0365266a4464277bae352b40afa6bbb920c6a717
Haohan Yang <yanghaohanmail@gmail.com>
2023-10-09 10:27:08 +0200

    seperate css and bootstrap js

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,5 @@
 import { Elm } from "../elm/Main.elm";
 
-import 'bootstrap/dist/js/bootstrap.bundle.js';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-
 const storedState = localStorage.getItem("elm-todo");
 const startingState = storedState ? JSON.parse(storedState) : null;
 const app = Elm.Main.init({ flags: startingState });
